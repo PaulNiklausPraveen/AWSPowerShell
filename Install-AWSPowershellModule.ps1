f461f8c42b6a79d99765e41d6358813e7396064b
Paul Niklaus Praveen <paulnikpraveen@gmail.com>
2023-03-26 05:49:32 +0530

    Create Install-AWSPowershellModule.ps1

---
+++ Install-AWSPowershellModule.ps1
@@ -0,0 +1,22 @@
+#Install Powershell Modules for AWS
+
+
+#Requires -Version  Powershell 7   
+
+If(!(Get-Module AWSPowerShell.NetCore))
+ { 
+ 
+Write-Host "`nInstalling AWSPowerShell.NetCore..." -Foreground Green
+
+Install-Module -name AWSPowerShell.NetCore -Scope CurrentUser -Force -AllowClobber -Verbose
+
+}
+
+Write-Host "`n`nImporting AWS Powershell module...`n`n"
+
+Import-Module AWSPowerShell.NetCore -Force -Verbose
+ 
+Get-AWSPowerShellVersion
+
+
+ 
