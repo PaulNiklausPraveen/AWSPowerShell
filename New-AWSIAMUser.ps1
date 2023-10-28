5be4bab10c48e3439fb2b671f6362ec8a3bd6e0a
Paul Niklaus Praveen <paulnikpraveen@gmail.com>
2023-10-29 01:53:20 +0530

    Create AWS IAM User

--- New-AWSIAMUser.ps1
+++ New-AWSIAMUser.ps1
@@ -16,8 +16,8 @@ Write-Host "Importing AWS .NetCore module...`n"
 Import-Module AWSPowerShell.NetCore -Force -Verbose
 
 Set-AWSCredential `
-                 -AccessKey AKIA4LHDMYQAAQPF32GZ `
-                 -SecretKey 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 `
+                 -AccessKey A8abca60b05880b86a1 `
+                 -SecretKey BNzxp8abca60bfm7305880b86a1 `
                  -StoreAs AWS-PaulProfile  #Type name of the profile you desire
 
 Set-AWSCredentials -ProfileName AWS-PaulProfile
